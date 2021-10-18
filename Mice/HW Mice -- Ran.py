57426c9d74f238a23a54e3df5280f8225d71f8de
Ranicruyn <90212198+Ranicruyn@users.noreply.github.com>
2021-10-18 04:05:22 -0400

    initialize

---
+++ Mice/HW Mice -- Ran.py
@@ -0,0 +1,8 @@
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(f"{1+2}\t\t\n\tBlind\t\n\t\tMice")
+3		
+	Blind	
+		Mice
+>>> 
